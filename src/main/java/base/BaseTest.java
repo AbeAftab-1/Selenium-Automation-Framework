44a000f08dff0afd5bff72e16d29907df7a6d65a
Abir Aftab <abiraftab@gmail.com>
2025-10-21 10:07:52 -0400

    log4j2 integration added

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,15 +5,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
+
 public class BaseTest {
 
 	protected WebDriver driver;
 	
 	@BeforeMethod 
 	public void setUp() {
-		
+		Log.info("Setting up WebDriver...");
 		driver = new ChromeDriver(); 
 		driver.manage().window().maximize();
+		Log.info("Navigating to URL...");
 		driver.get("https://admin-demo.nopcommerce.com/login");
 	}
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,20 +5,24 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest{
 	
 	@Test
 	public void testValidLogin() {
-		
+		Log.info("Starting valid login test...");
 		LoginPage validLogin = new LoginPage(driver);
 		
+		Log.info("Adding credentials...");
 		validLogin.enterUserName("admin@yourstore.com");
 		validLogin.enterPassword("admin");
 		validLogin.clickLogin();
 		
+		Log.info("Verifying page title...");
 		System.out.println("Title for the page is: "+driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+		Log.info("Valid login test completed...");
 	}
 
 }
